3679735f32eb7df71bbff828e7411f26b492371f
Scott <scottd564@users.noreply.github.com>
2020-12-18 18:29:16 +0000

    list value by name and print all

--- Program.cs
+++ Program.cs
@@ -11,22 +11,40 @@ namespace DMXReader
            return new string('\t', i);
         }
 
-        static void PrintElements(DXElement[] elements, int depth = 0)
+        static List<object> PrintElement(DXElement element, string name = null)
+        {
+            List<object> foundAttributeValues = new List<object>();
+
+            Console.WriteLine($"[Root Element: {element.Id} | {element.Type} - {element.Name}]");
+            PrintAttributes(element.Attributes, depth: 0, find: name, foundAttributeValues: ref foundAttributeValues);
+
+            return foundAttributeValues;
+        }
+
+        static void PrintElements(DXElement[] elements, ref List<object> foundAttributeValues, int depth = 0, string find = null)
         {
             foreach (DXElement element in elements)
             {
                 Console.WriteLine($"{Tab(depth)}[Element: {element.Id} | {element.Type} - {element.Name}]");
-                PrintAttributes(element.Attributes, depth);
+                PrintAttributes(element.Attributes, depth: depth, find: find, foundAttributeValues: ref foundAttributeValues);
             }
         }
 
-        static void PrintAttributes(List<DXAttribute> attributes, int depth = 0)
+        static void PrintAttributes(List<DXAttribute> attributes, ref List<object> foundAttributeValues, int depth = 0, string find = null)
         {
             foreach (DXAttribute attribute in attributes)
             {
                 if (attribute.Type != DmAttributeType_t.AT_ELEMENT || attribute.Type != DmAttributeType_t.AT_ELEMENT_ARRAY)
                 {
-                    Console.WriteLine($"{Tab(depth)}\t[Attributes: {attribute.Name} - {attribute.Value}]");
+                    if (find != null && attribute.Name.Contains(find))
+                    {
+                        if (!foundAttributeValues.Contains(attribute.Value))
+                            foundAttributeValues.Add(attribute.Value);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{Tab(depth)}\t[Attributes: {attribute.Name} - {attribute.Value}]");
+                    }
                 }
             }
 
@@ -34,11 +52,11 @@ namespace DMXReader
             {
                 if (attribute.Type == DmAttributeType_t.AT_ELEMENT)
                 {
-                    PrintElements(new DXElement[1] { (DXElement)attribute.Value }, depth++);;
+                    PrintElements(new DXElement[1] { (DXElement)attribute.Value }, depth: depth++, find: find, foundAttributeValues: ref foundAttributeValues);
                 }
                 else if (attribute.Type == DmAttributeType_t.AT_ELEMENT_ARRAY)
                 {
-                    PrintElements((DXElement[])attribute.Value, depth++);
+                    PrintElements((DXElement[])attribute.Value, depth: depth++, find: find, foundAttributeValues: ref foundAttributeValues);
                 }
             }
         }
@@ -60,9 +78,37 @@ namespace DMXReader
 #endif
 
             DXElement root = dmx.Unserialize();
-            PrintElements(new DXElement[1] { root });
 
-            Console.ReadLine();
+            Console.WriteLine("Type exit, print or list");
+
+            bool cycle = true;
+            while(cycle)
+            {
+                string input = Console.ReadLine().ToLower();
+                switch(input)
+                {
+                    case "exit":
+                        cycle = false;
+                        break;
+                    case "print":
+                        PrintElement(root);
+                        break;
+                    case "list":
+                        Console.Write("Type the attribute name to list: ");
+                        string name = Console.ReadLine();
+                        List<object> values = PrintElement(root, name);
+                        string output = "";
+
+                        foreach (object value in values)
+                        {
+                            Console.WriteLine(value);
+                            output += value + Environment.NewLine;
+                        }
+
+                        System.IO.File.WriteAllText("output.txt", output);
+                        break;
+                }
+            }
         }
     }
 }
